30a37fb6da7ae8179f6497306ec783ca0a774f84
KarelHernz <karelhernandez29@gmail.com>
2025-11-08 14:30:40 +0000

    JavaFx-25 added

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -1,4 +1,24 @@
 package view;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class MainApp extends Application {
+    public static void main(String[] args) {launch(args);}
+
+
+    //Janela do programa
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ControlPanel.fxml")));
+        var primaryStage = new Stage();
+        primaryStage.setTitle("Hello world");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 }
