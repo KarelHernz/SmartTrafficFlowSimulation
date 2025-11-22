930f840600859684d3ca07469d927761817c4114
KarelHernz <karelhernandez29@gmail.com>
2025-11-22 20:10:23 +0000

    State Pattern

--- src/model/state/Red.java
+++ src/model/state/Red.java
@@ -1,4 +1,30 @@
 package model.state;
 
-public class Red {
+import controller.State;
+import model.TrafficLight;
+
+public class Red implements State {
+    private final double tempo = 8.0;
+
+    @Override
+    public void enter(TrafficLight trafficLight) {
+        trafficLight.setCor(trafficLight.Cor.RED);
+        trafficLight.resetTempo();
+    }
+
+    @Override
+    public void exit(TrafficLight trafficLight) {}
+
+    @Override
+    public void update(TrafficLight trafficLight, double deltaTime) {
+        trafficLight.incrementTempo(deltaTime);
+        if (trafficLight.getTempo() >= tempo) {
+            trafficLight.setState(new Green());
+        }
+    }
+
+    @Override
+    public TrafficLight.Cor getColor() {
+        return TrafficLight.Cor.RED;
+    }
 }
